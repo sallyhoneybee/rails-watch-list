57bfb59d229d3608d8716fe0ec0f82a8f52d289f
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-10 23:46:51 +0800

    Added photo upload and attribute for list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class ListsController < ApplicationController
   def index
     @lists = List.all
@@ -23,6 +25,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
