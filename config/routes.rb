78e5360835a6bef795b9371dcf038bd083fe229a
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-10 12:39:06 +0800

    Added rooth to lists#index in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index show new create] do
+  root to: 'lists#index'
+  resources :lists, only: %i[show new create] do
     resources :bookmarks, only: %i[new create]
   end
 
